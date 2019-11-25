8ef11918cc1815d67df5f6e1527a3501397c4b18
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-11-25 03:21:49 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
 def addition(num1, num2)
-
+  puts "(addition(5,4)).to eq(9)"
 end
 
 def subtraction(num1, num2)
-
+puts "(subtraction(10,5)).to eq(5)"
 end
 
 def division(num1, num2)
-
+puts "(division(50,2)).to eq(25)"
 end
 
 def multiplication(num1, num2)
-
+puts "(multiplication(4,30)).to eq(120)"
 end
 
 def modulo(num1, num2)
-
+puts "(modulo(34, 5)).to eq(4)"
 end
 
 def square_root(num)
-
+puts "(square_root(81)).to eq(9)"
 end
+
+addition(num1, num2)
+subtraction(num1, num2)
+square_root(num)
+modulo(num1, num2)
+multiplication(num1, num2)
+division(num1, num2)
